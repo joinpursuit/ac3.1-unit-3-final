baf91e480d7212ea5e449592703d669a18c94ed6
Rabiya Watson-Maiorana <ormabe@nyc.rr.com>
2016-10-14 16:30:51 -0400

    ugh

---
+++ src/actions.js
@@ -0,0 +1,21 @@
+export const ADD_ITEMS = "ADD_ITEMS";
+
+export const addShirt = () => ({
+	type: "ADD_ITEMS",
+	item: "Shirt"
+})
+
+export const addShoes = () => ({
+	type: "ADD_ITEMS",
+	item: "Shoes"
+})
+
+export const addHat = () => ({
+	type: "ADD_ITEMS",
+	item: "Hat"
+})
+
+export const addSunglasses = () => ({
+	type: "ADD_ITEMS",
+	item: "Sunglasses"
+})
