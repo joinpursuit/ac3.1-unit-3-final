23fc4556ed7e5f17779032c9d8a0adde2119579c
Ambar Gutierrez <ambar.grz@gmaail.com>
2016-10-14 16:30:41 -0400

    exam 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import  ClothContainer from'./component/cloths_containers'
+import Store from './store';
 
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    )
-  }
-})
+const App= ()=> (
+	<Provider store= {Store}>
+		
+		<ClothContainer />
+	</Provider>
+)
+// var App = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <h1>Hello World!</h1>
+//       </div>
+//     )
+//   }
+// })
 
-ReactDOM.render(
-  <HelloWorld />,
+const render = () => ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
+
+render();
+store.subscribe(render);
+
+
+// document.addEventListener("DOMContentLoaded", ()=> {
+// 	ReactDOM.render(
+// 		<App />,
+// 		document.getElementById('root')
+// 	);
+// });
